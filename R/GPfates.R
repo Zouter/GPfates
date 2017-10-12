a235924aabade9bdc32e9328c61156b7e4aa3abf
Robrecht Cannoodt <brechtus@gmail.com>
2017-10-12 16:57:46 +0200

    arguments as json

--- R/GPfates.R
+++ R/GPfates.R
@@ -5,6 +5,7 @@
 #' @param ndims The number of dimensions
 #' @param log_expression_cutoff The log expression cutoff
 #' @param min_cells_expression_cutoff The min expression cutoff
+#' @param verbose Print output produced by method
 #'
 #' @export
 #' @importFrom glue glue
@@ -15,35 +16,44 @@ GPfates <- function(
   nfates = 1,
   ndims = 2,
   log_expression_cutoff = 2,
-  min_cells_expression_cutoff = 2
+  min_cells_expression_cutoff = 2,
+  verbose = FALSE
 ) {
-  temp_folder <- tempdir()
+  # create temporary folder
+  temp_folder <- tempfile()
+  dir.create(temp_folder, recursive = TRUE)
 
-  utils::write.table(t(counts), paste0(temp_folder, "expression.csv"), sep="\t")
+  # write counts to temporary file
+  utils::write.table(t(counts), paste0(temp_folder, "/expression.tsv"), sep="\t")
 
-  utils::write.table(
-    data.frame(cell_id = rownames(counts), row.names = rownames(counts)),
-    paste0(temp_folder, "cellinfo.csv"),
-    sep="\t"
-  )
+  # write cell info to temporary file
+  cell_info <- data.frame(cell_id = rownames(counts), row.names = rownames(counts))
+  utils::write.table(cell_info, paste0(temp_folder, "/cellinfo.tsv"), sep="\t")
 
-  system2(
+  # write parameters to temporary folder
+  params <- as.list(environment())[formalArgs(GPfates)]
+  params <- params[names(params) != "counts"]
+  write(jsonlite::toJSON(params, auto_unbox = TRUE), paste0(temp_folder, "/params.json"))
+
+  # execute python script
+  output <- system2(
     "/bin/bash",
     args = c(
       "-c",
       shQuote(glue::glue(
         "cd {find.package('GPfates')}/venv",
         "source bin/activate",
-        "python3 {find.package('GPfates')}/wrapper.py {temp_folder} {log_expression_cutoff} {min_cells_expression_cutoff} {nfates} {ndims}",
+        "python3 {find.package('GPfates')}/wrapper.py {temp_folder}",
         .sep = ";"))
     )
   )
 
-  pseudotime <- readr::read_csv(glue::glue("{temp_folder}pseudotimes.csv"), col_names = c("cell_id", "time"))
+  if (verbose) cat(output, "\n", sep="")
 
-  phi <- readr::read_csv(glue::glue("{temp_folder}phi.csv"), col_names = c("cell_id", glue::glue("M{seq_len(nfates)}")), skip = 1)
-
-  dr <- readr::read_csv(glue::glue("{temp_folder}dr.csv"), col_names = c("cell_id", glue::glue("Comp{seq_len(5)}")), skip = 1)
+  # read output
+  pseudotime <- readr::read_csv(glue::glue("{temp_folder}/pseudotimes.csv"), col_names = c("cell_id", "time"))
+  phi <- readr::read_csv(glue::glue("{temp_folder}/phi.csv"), col_names = c("cell_id", glue::glue("M{seq_len(nfates)}")), skip = 1)
+  dr <- readr::read_csv(glue::glue("{temp_folder}/dr.csv"), col_names = c("cell_id", glue::glue("Comp{seq_len(5)}")), skip = 1)
 
   # remove temporary output
   unlink(temp_folder, recursive = TRUE)
--- inst/wrapper.py
+++ inst/wrapper.py
@@ -1,21 +1,19 @@
 import pandas as pd
 import numpy as np
-
+import sys
 from GPfates import GPfates
 
-import sys
-
 temp_folder = sys.argv[1]
-log_expression_cutoff = float(sys.argv[2])
-min_cells_expression_cutoff = float(sys.argv[3])
-nfates = int(sys.argv[4])
-ndims = int(sys.argv[5])
 
-etpm = pd.read_table(temp_folder + 'expression.csv', index_col=0)
-etpm = etpm[(etpm > log_expression_cutoff).sum(1) >min_cells_expression_cutoff]
+# Load params
+p = json.load(open(temp_folder + "/params.json", "r"))
+
+# load data
+etpm = pd.read_table(temp_folder + "/expression.tsv", index_col=0)
+etpm = etpm[(etpm > p["log_expression_cutoff"]).sum(1) > p["min_cells_expression_cutoff"]]
 logexp = np.log10(etpm + 1)
 
-cellinfo = pd.read_table(temp_folder + 'cellinfo.csv', index_col=0)
+cellinfo = pd.read_table(temp_folder + "/cellinfo.tsv", index_col=0)
 
 m = GPfates.GPfates(cellinfo, logexp)
 
@@ -23,20 +21,20 @@ print("Dimensionality reduction--------------------------------------")
 m.dimensionality_reduction()
 
 print("Story DR------------------------------------------------------")
-m.store_dr(dims=range(ndims)) # store the dr in the sample table (m.s), so it can be used in the gplvm
+m.store_dr(dims=range(p["ndims"])) # store the dr in the sample table (m.s), so it can be used in the gplvm
 
 print("Infer pseudotime----------------------------------------------")
-m.infer_pseudotime(s_columns=['bgplvm_' + str(i) for i in range(ndims)]) # use the first two components to infer pseudotime
+m.infer_pseudotime(s_columns=["bgplvm_" + str(i) for i in range(ndims)]) # use the first two components to infer pseudotime
 
 print("Model cell fates----------------------------------------------")
-m.model_fates(C=nfates)
+m.model_fates(C=p["nfates"])
 
 print("Saving--------------------------------------------------------")
-m.s.pseudotime.to_csv(temp_folder + "pseudotimes.csv")
-pd.DataFrame(m.fate_model.phi, index=m.s.pseudotime.index).to_csv(temp_folder + "phi.csv")
+m.s.pseudotime.to_csv(temp_folder + "/pseudotimes.csv")
+pd.DataFrame(m.fate_model.phi, index=m.s.pseudotime.index).to_csv(temp_folder + "/phi.csv")
 
 dr = m.dr_models["bgplvm"]
 dr2 = dr.X.mean[:, :]
-pd.DataFrame(dr2.tolist(), index=m.s.pseudotime.index).to_csv(temp_folder + "dr.csv")
+pd.DataFrame(dr2.tolist(), index=m.s.pseudotime.index).to_csv(temp_folder + "/dr.csv")
 
 print("Finished------------------------------------------------------")
